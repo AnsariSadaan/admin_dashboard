0fb674bd5ff2dfc43ed41c79d894fc3a1394cd8f
Ansari Sadaan <ansarisadaan72@gmail.com>
2024-12-17 18:08:51 +0530

    remove balnk spaces

--- app/Views/Home.php
+++ app/Views/Home.php
@@ -8,10 +8,8 @@
                         <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
                                 class="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
                     </div>
-
                     <!-- Content Row -->
                     <div class="row">
-
                         <!-- Earnings (Monthly) Card Example -->
                         <div class="col-xl-3 col-md-6 mb-4">
                             <div class="card border-left-primary shadow h-100 py-2">
